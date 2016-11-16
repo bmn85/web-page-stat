32d6d26c229b5f5703a308916e3160a7244d4fc2
MNB <mnb.anonimus@gmail.com>
2016-11-17 01:00:44 +0300

    init class and test

---
+++ web-page-stat-lib/src/main/java/ru/bmn/webpagestat/WebPage.java
@@ -0,0 +1,12 @@
+package ru.bmn.webpagestat;
+
+/**
+ *
+ */
+public class WebPage {
+	private final String url;
+
+	public WebPage(String url) {
+		this.url = url;
+	}
+}
---
+++ web-page-stat-lib/src/test/ru/bmn/webpagestat/WebPageTest.java
@@ -0,0 +1,16 @@
+package ru.bmn.webpagestat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class WebPageTest {
+	@Test
+	public void something() {
+		assertTrue(true);
+	}
+
+}
